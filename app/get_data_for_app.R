5ad21ded9ddc70a343baad5531af6e4d2b0276bd
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2019-11-15 16:01:42 -0800

    Update dates selected and sector names

--- app/get_data_for_app.R
+++ app/get_data_for_app.R
@@ -65,14 +65,15 @@ provinces <- data_20_10_0008 %>%
   ## Extract Canada and provinces (not territories), Seasonally Adjusted Retail Trade
   filter(vector %in% c("v52367097", "v52367155", "v52367185", "v52367215", "v52367245",
                        "v52367394", "v52367424", "v52367454", "v52367484", "v52367514",
-                       "v52367573")) %>%
+                       "v52367573"),
+         as.Date(ref_date) %in% c((max(ref_date) - years(5)):max(ref_date))) %>%
   group_by(geo) %>%
   get_mom_stats() %>%
   select(ref_date, geo,
-         value, mom_pct, yoy_pct) %>%
+         value, mom_pct) %>%
   ungroup()
 
-write_rds(provinces, here("data","provinces.rds"))
+write_rds(provinces, here("app","data","provinces.rds"))
 
 
 # Filter sector data for app ----
@@ -81,17 +82,18 @@ sectors <-
   data_20_10_0008 %>%
   filter(geo == "British Columbia",
          adjustments %in% c("Unadjusted"),
+         as.Date(ref_date) %in% c((max(ref_date) - years(5)):max(ref_date)),
          str_detect(
-           classification_code_for_north_american_industry_classification_system_naics, "\\[4..\\]")) %>%
+           classification_code_for_north_american_industry_classification_system_naics,
+           "\\[4..\\]|\\[44-45\\]")) %>%
   group_by(classification_code_for_north_american_industry_classification_system_naics) %>%
   get_yoy_stats() %>%
-  mutate(Subsector =
-           str_remove(north_american_industry_classification_system_naics,
-                      "\\[4..\\]") %>%
-           strwrap(width = 8) %>% paste0(collapse = "\n")) %>%
+  mutate(sector = str_remove(north_american_industry_classification_system_naics,
+                      "\\[4..\\]|\\[44-45\\]")) %>%
+  mutate(sector = ifelse(sector == "Retail trade ", "All Retail Trade",sector)) %>%
   ungroup() %>%
-  select(ref_date, Subsector,
-         value, mom_pct, yoy_pct)
+  select(ref_date, sector,
+         value, yoy_pct)
 
-write_rds(sectors, here("data","sectors.rds"))
+write_rds(sectors, here("app","data","sectors.rds"))
 
