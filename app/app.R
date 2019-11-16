004d7a8050e72f140ecdd622ff2f53033d4d6c67
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2019-11-15 16:02:02 -0800

    App overhaul

--- app/app.R
+++ app/app.R
@@ -13,7 +13,7 @@
 #####
 # METADATA for app
  # dataVersion <- "Retail Trade"
- # updateDate <- "Nov 2019"
+  updateDate <- "Nov 2019"
 
 ## load libraries  ----
 ## installs any missing packages this script uses
@@ -33,20 +33,23 @@ library(plotly)
 #data1 <- readRDS("data/data1.rds")  ## by single-year intervals
 
 provinces <- readRDS("data/provinces.rds")
+sectors <- readRDS("data/sectors.rds")
 
-dates <- provinces %>%
-  filter(as.Date(ref_date) %in% c((latest_month - years(1)):latest_month)) %>%
-  select(ref_date) %>%
-  unique() %>%
-  arrange(desc(ref_date)) %>%
-  pull()
+## chart theme ----
+bcstats_chart_theme <-
+  theme_bw() +
+  theme(
+    panel.border = element_rect(colour="white"),
+    plot.title = element_text(face="bold"),
+    legend.position=c(1,0),
+    legend.justification=c(1,0),
+    legend.title = element_text(size=12),
+    legend.text = element_text(size=11),
+    axis.line = element_line(colour="black"),
+    axis.title = element_text(size=12),
+    axis.text = element_text(size=10)
+  )
 
-names(dates) <- paste(month(dates, label = TRUE), year(dates))
-
-geos <- provinces %>%
-  select(geo) %>%
-  unique() %>%
-  pull()
 
 # UI demonstrating column layouts
 ui <- fluidPage(title = "BC Retail Sales",
@@ -66,66 +69,76 @@ ui <- fluidPage(title = "BC Retail Sales",
              )
            )
     ),
-    column(width=12,
-           style = "margin-top:100px",
-            tags$fieldset(
-                  tags$legend(h2("Some heading here")),
-                  p("Some text in a paragraph here.",
-                  style="font-size:14px; color:#494949"),
-                  br()
-            )
-    ),
+    # column(width=12,
+    #        style = "margin-top:100px",
+    #         tags$fieldset(
+    #               tags$legend(h2("Some heading here")),
+    #               p("Some text in a paragraph here.",
+    #               style="font-size:14px; color:#494949"),
+    #               br()
+    #         )
+    # ),
     ## Make changes to this column
     column(width = 12,
+           style = "margin-top:100px",
            sidebarLayout(
              sidebarPanel(
-               style="background-color:#F2F2F2;",
-               width = 3,
+               #width = 3, ## Define width in style instead
+               style="background-color:#F2F2F2;position:fixed;width:300px",
                tags$fieldset(
-                 tags$legend(h3("Date selection")),
-                 selectInput(
-                   inputId = "date",
-                   label = "Date",
-                   choices = dates,
-                   selectize = FALSE,
-                   size = 3
-                   )
+                 tags$legend(h3("Date")),
+                 uiOutput(outputId = "date")
                  ),
                tags$fieldset(
-                 tags$legend(h3("Monthly Change")),
-                 h3(uiOutput(outputId = "BC_mom", align = "center"))
-                 ),
-               br(),
+                 tags$legend(h3("Geography")),
+                 uiOutput(outputId = "geo")
+               ),
                tags$fieldset(
-                 tags$legend(h3("Yearly Change")),
-                 h3(uiOutput(outputId = "BC_yoy", align = "center"))
+                 tags$legend(h3("Sector")),
+                 uiOutput(outputId = "sector")
+               ),
+               tags$fieldset(
+                 tags$legend(h3("Percent Change")),
+                 h3(uiOutput(outputId = "pct_chg"))
                  ),
                br(),
+               tags$fieldset(
+                 tags$legend(h4("Additional information")),
+                 uiOutput(outputId = "info"),
+                 HTML(paste0("<br><br> Sources: Table <a href='https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010000801'>20-10-0008-01</a>",
+                             " <br>", "Code for this app: <a href = 'https://github.com/bcgov/CANSIM-retail-trade-shiny'>Github</a>",
+                             " <br>", "Last updated: ", updateDate))
+                 ),
                br(),
                br()
                ),
            mainPanel(
-             width = 9,
-             tabsetPanel(
+             #width = 9,
+             style = "margin-left:320px",
+             tabsetPanel(id = "tabs",
                tabPanel("Provinces",
                         tags$fieldset(
                           style = "margin-top:20px;",
-                          tags$legend(h3("Month-Over-Month Change in Retail Sales")),
+                          tags$legend(h3("Month-Over-Month Change in All Retail Trade Sales")),
                           plotlyOutput(outputId = "provinces_mom")
                          ),
                         tags$fieldset(
-                          style = "margin-top:20px;",
-                          tags$legend(h3("Value of Retail Sales")),
-                          selectInput(
-                            inputId = "geo",
-                            label = "Geography",
-                            choices = geos,
-                            selected = "British Columbia"
-                          ),
+                       #   style = "margin-top:20px;",
+                          tags$legend(h3("Value of All Retail Trade Sales")),
                           plotlyOutput(outputId = "provinces_value")
                         )
                        ),
-               tabPanel("Sectors"
+               tabPanel("Sectors",
+                        tags$fieldset(
+                          style = "margin-top:20px;",
+                          tags$legend(h3("Year-Over-Year Change in Retail Sales in BC")),
+                          plotlyOutput(outputId = "sectors_yoy")
+                        ),
+                        tags$fieldset(
+                        #  style = "margin-top:20px;",
+                          tags$legend(h3("Value of Retail Sales in BC")),
+                          plotlyOutput(outputId = "sectors_value")
+                        )
 
                         ),
                type = "tabs"
@@ -155,40 +168,179 @@ ui <- fluidPage(title = "BC Retail Sales",
 ## Define server logic ----
 server <- function(input, output, session) {
 
-  output$BC_mom <- renderUI({
 
-    val <- provinces %>%
-      filter(ref_date == input$date,
-             geo == input$geo) %>%
-      pull(mom_pct) %>%
-      round_half_up(digits = 1) %>%
-      paste0("%")
+  get_inputs <- reactive({
 
-    icon <- ifelse(val > 0, "arrow-alt-circle-up", "arrow-alt-circle-down")
+    req(input$tabs)
 
-    tagList(
-      HTML(val),
-      icon(icon)
+    if(input$tabs == "Provinces") {
+      date <- provinces %>%
+        filter(as.Date(ref_date) %in% c((latest_month - years(1)):latest_month)) %>%
+        select(ref_date) %>%
+        unique() %>%
+        arrange(desc(ref_date)) %>%
+        pull()
+
+      names(date) <- paste(month(date, label = TRUE), year(date))
+
+      geos <- provinces %>%
+        select(geo) %>%
+        unique() %>%
+        pull()
+
+      sectors <- c("All Retail Trade")
+
+      change <- "Monthly"
+      adjustment <- "seasonally adjusted"
+
+    }
+
+    else if(input$tabs == "Sectors") {
+
+      date <- sectors %>%
+        filter(as.Date(ref_date) %in% c((latest_month - years(1)):latest_month)) %>%
+        select(ref_date) %>%
+        unique() %>%
+        arrange(desc(ref_date)) %>%
+        pull()
+
+      names(date) <- paste(month(date, label = TRUE), year(date))
+
+      geos <- "British Columbia"
+
+      sectors <- sectors %>%
+        select(sector) %>%
+        unique() %>%
+        pull()
+
+      change <- "Yearly"
+      adjustment <- "unadjusted"
+    }
+
+    list(date = date,
+         geos = geos,
+         sectors = sectors,
+         change = change,
+         adjustment = adjustment)
+
+  })
+
+  output$date <- renderUI({
+
+    selectInput(
+      inputId = "date",
+      label = NULL,
+      choices = get_inputs()$date,
+      selectize = FALSE,
+      size = 3
     )
 
   })
-  ## Should make a reactive for filtering the data so only done once and not
-  ## in each render
 
-  ## Does it make sense to do yearly change on seasonally adjusted data?
-  output$BC_yoy <- renderUI({
+  output$geo <- renderUI({
 
-    val <- provinces %>%
-      filter(ref_date == input$date,
-             geo == input$geo) %>%
-      pull(yoy_pct) %>%
-      round_half_up(digits = 1) %>%
-      paste0("%")
+    selectInput(
+      inputId = "geo",
+      label = NULL,
+      choices = get_inputs()$geos,
+      selected = "British Columbia"
+    )
+
+  })
+
+  output$sector <- renderUI({
+
+    selectInput(
+      inputId = "sector",
+      label = NULL,
+      choices = get_inputs()$sectors,
+      selected = "All Retail Trade"
+    )
+
+  })
+
+  output$info <- renderUI({
+
+    HTML(paste0("All numbers and figures on this tab are based on <b>",
+                get_inputs()$adjustment, "</b> estimates."))
+
+  })
+
+  get_data <- reactive({
+
+    req(input$tabs)
+    req(input$date)
+    req(input$geo)
+    req(input$sector)
+
+    if(input$tabs == "Provinces"){
+
+      pct_chg <- provinces %>%
+        filter(ref_date == input$date,
+               geo == input$geo) %>%
+        pull(mom_pct)
+
+      prov_bar_chart <- provinces %>%
+        filter(ref_date == input$date) %>%
+        mutate(order = case_when(geo == "British Columbia" ~ 1,
+                                 geo == "Canada" ~ 2,
+                                 TRUE ~ rank(desc(mom_pct)) + 2),
+               geo = str_replace_all(geo, " ", "\n")) %>%
+        arrange(order) %>%
+        mutate(geo = factor(geo, levels = geo))
+
+      prov_line_chart <- provinces %>%
+            filter(geo == input$geo) %>%
+            mutate(value = value/1000000)
+
+      sect_bar_chart <- NULL
+
+      sect_line_chart <- NULL
+
+    }
+
+    else if(input$tabs == "Sectors"){
+
+      pct_chg <- sectors %>%
+        filter(ref_date == input$date,
+               sector == input$sector) %>%
+        pull(yoy_pct)
+
+      prov_bar_chart <- NULL
+
+      prov_line_chart <- NULL
+
+      sect_bar_chart <- sectors %>%
+        filter(ref_date == input$date) %>%
+        mutate(order = case_when(sector == "All Retail Trade" ~ 1,
+                                 TRUE ~ rank(desc(yoy_pct)) + 1),
+               sector = str_replace_all(sector, " ", "\n")) %>%
+        arrange(order) %>%
+        mutate(sector = factor(sector, levels = sector))
+
+      sect_line_chart <- sectors %>%
+        filter(sector == input$sector) %>%
+        mutate(value = value/1000000)
+
+    }
+
+    list(pct_chg = pct_chg,
+         prov_bar_chart = prov_bar_chart,
+         prov_line_chart = prov_line_chart,
+         sect_bar_chart = sect_bar_chart,
+         sect_line_chart = sect_line_chart)
+
+  })
+
+  output$pct_chg <- renderUI({
+
+    val <- get_data()$pct_chg %>%
+      round_half_up(digits = 1)
 
     icon <- ifelse(val > 0, "arrow-alt-circle-up", "arrow-alt-circle-down")
 
     tagList(
-      HTML(val),
+      HTML(paste0(get_inputs()$change,": ",val, "%")),
       icon(icon)
     )
 
@@ -196,56 +348,105 @@ server <- function(input, output, session) {
 
   output$provinces_mom <- renderPlotly({
 
-    plot_data <- provinces %>%
-      filter(ref_date == input$date) %>%
-      mutate(order = case_when(geo == "British Columbia" ~ 1,
-                               geo == "Canada" ~ 2,
-                               TRUE ~ rank(desc(mom_pct)) + 2)) %>%
-      arrange(order) %>%
-      mutate(geo = factor(geo, levels = geo)) ## To ensure bars ordered in chart correctly
+    if(is.null(get_data()$prov_bar_chart)) {
+      NULL
+    }
 
-    p <- ggplot(plot_data,
-                aes(x = geo, y = mom_pct)) +
-      geom_bar(stat = "identity") +
-      geom_hline(yintercept = 0) +
-      geom_vline(xintercept = 2.5) +
-      labs(x = NULL,
-           y = "% Change",
-           title = "By Province") +
-      theme_classic() +
-      theme(axis.line.x = element_blank(),
-            axis.text.x = element_text(angle = 25))
+    else{
+      p <- ggplot(get_data()$prov_bar_chart,
+                  aes(x = geo, y = mom_pct)) +
+        geom_bar(stat = "identity") +
+        geom_hline(yintercept = 0) +
+        geom_vline(xintercept = 2.5) +
+        labs(x = NULL,
+             y = "% Change",
+             title = "By Province") +
+        bcstats_chart_theme
+       # theme_classic() +
+        #theme(axis.line.x = element_blank())
 
-    p <- ggplotly(p)
+      p <- ggplotly(p)
+    }
 
   })
 
-  ## Should this chart be done as an index to be able to compare across provinces?
   output$provinces_value <- renderPlotly({
 
-    plot_data <- provinces %>%
-      filter(geo == input$geo,
-             as.Date(ref_date) %in% c((latest_month - years(5)):latest_month)) %>%
-      mutate(value = value/1000000)
+    if(is.null(get_data()$prov_line_chart)){
+      NULL
+    }
 
-    p <- ggplot(plot_data, aes(x = ref_date, y = value)) +
-      geom_line() +
-      labs(x = NULL,
-           y = "Billions of Dollars",
-           #title = input$geo) +
-           title = "British Columbia") +
-      theme_classic() +
-      scale_x_date(limits = c(latest_month - years(5), latest_month),
-                   expand = c(0,0),
-                   date_breaks = "6 months",
-                   date_labels = "%b %Y" ) +
-      theme(axis.text.x = element_text(angle = 15))
+    else{
 
-    p <- ggplotly(p)
+      p <- ggplot(get_data()$prov_line_chart,
+                  aes(x = ref_date, y = value)) +
+        geom_line() +
+        labs(x = NULL,
+             y = "Billions of Dollars",
+             title = paste("for",input$geo)) +
+        bcstats_chart_theme +
+        scale_x_date(limits = c(max(get_data()$prov_line_chart$ref_date) - years(5),
+                                max(get_data()$prov_line_chart$ref_date) + months(3)),
+                     expand = c(0,0),
+                     date_breaks = "6 months",
+                     date_labels = "%b\n%Y" )
 
-    p$x$data[[1]]$text <- paste0(p$x$data[[1]]$text, "<br />yoy_pct: ", plot_data$yoy_pct)
+      p <- ggplotly(p)
 
-    p
+    }
+
+
+  })
+
+  output$sectors_yoy <- renderPlotly({
+
+    if(is.null(get_data()$sect_bar_chart)) {
+      NULL
+    }
+
+    else{
+      p <- ggplot(get_data()$sect_bar_chart,
+                  aes(x = sector, y = yoy_pct)) +
+        geom_bar(stat = "identity") +
+        geom_hline(yintercept = 0) +
+        geom_vline(xintercept = 1.5) +
+        labs(x = NULL,
+             y = "% Change",
+             title = "By Sector") +
+        bcstats_chart_theme
+        # theme_classic() +
+        # theme(axis.line.x = element_blank())
+
+      p <- ggplotly(p)
+
+    }
+  })
+
+  output$sectors_value <- renderPlotly({
+
+    if(is.null(get_data()$sect_line_chart)){
+      NULL
+    }
+
+    else{
+
+      p <- ggplot(get_data()$sect_line_chart,
+                  aes(x = ref_date, y = value)) +
+        geom_line() +
+        labs(x = NULL,
+             y = "Billions of Dollars",
+             title = paste("for",input$sector)) +
+        bcstats_chart_theme +
+        scale_x_date(limits = c(max(get_data()$sect_line_chart$ref_date) - years(5),
+                                max(get_data()$sect_line_chart$ref_date) + months(3)),
+                     expand = c(0,0),
+                     date_breaks = "6 months",
+                     date_labels = "%b\n%Y" )
+
+      p <- ggplotly(p)
+
+    }
+
 
   })
 
